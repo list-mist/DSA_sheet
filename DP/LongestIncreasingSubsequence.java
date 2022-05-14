06c72be02a11972f95d6c604b2c940bbb6d83b80
list-mist <87711815+list-mist@users.noreply.github.com>
2022-05-14 21:40:34 +0530

    Update LongestIncreasingSubsequence.java

--- DP/LongestIncreasingSubsequence.java
+++ DP/LongestIncreasingSubsequence.java
@@ -24,4 +24,23 @@ class Solution {
     }
     // Time complexity ->  O(N*N)
     // Space complexity -> O(N*N)
+    int maxLIS(int[] a, int n){
+        int[] dp = new int[n];
+        int max = 1;
+      
+        Arrays.fill(dp,1);
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(a[i] > a[j]){
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+               
+            }
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+    }
+}
+     // Time complexity ->  O(N*N)
+    // Space complexity -> O(N)
 }

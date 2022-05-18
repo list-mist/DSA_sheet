e03c25c3bbebde9ae5481d497c0af91568d0fef9
list-mist <87711815+list-mist@users.noreply.github.com>
2022-05-18 22:46:43 +0530

    Create ChocolateDistributionProblem.java

---
+++ Arrays/ChocolateDistributionProblem.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public long findMinDiff(ArrayList<Integer> a, int n, int m)
+    {
+        Collections.sort(a);
+        int min = Integer.MAX_VALUE;
+        int i = 0, j = 0;
+        while(j < n){
+            if(j < i + m - 1) j++;
+            else if(j == i + m -1){
+                min = Math.min(min, a.get(j) - a.get(i));
+                i++;
+                j++;
+            }
+        }
+        return min;
+    }
+    
+}
